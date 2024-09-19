1001540979e39b08e6c05a6e6b69ae5dab69420b
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-09-19 15:20:37 -0300

    Create Entity Client

---
+++ src/main/java/com/compasso/demo_park_api/entity/Client.java
@@ -0,0 +1,63 @@
+package com.compasso.demo_park_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "tb_clients")
+@EntityListeners(AuditingEntityListener.class)
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "cpf", nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @OneToOne
+    @JoinColumn(name = "id_user", nullable = false)
+    private User user;
+
+    @CreatedDate
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @LastModifiedDate
+    @Column(name = "modification_date")
+    private LocalDateTime modificationDate;
+
+    @CreatedBy
+    @Column(name = "create_by")
+    private String createBy;
+
+    @LastModifiedBy
+    @Column(name = "modify_by")
+    private String modifyBy;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
